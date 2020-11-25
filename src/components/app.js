c12886836179996af889ef9dd919e59740823e95
Myra James <Treesmellgood@gmail.com>
2020-11-25 10:49:17 -0500

    added render props

--- src/components/app.js
+++ src/components/app.js
@@ -12,16 +12,50 @@ import Auth from "./pages/auth";
 import noMatch from "./pages/noMatch";
 
 export default class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      loggedInStatus: "NOT_LOGGED_IN"
+    };
+
+    this.handleSuccessfullLogin = this.handleSuccessfullLogin.bind(this);
+    this.handleUnSuccessfulLogin = this.handleUnSuccessfulLogin.bind(this);
+  }
+
+  handleSuccessfullLogin() {
+    this.setState({
+      loggedInStatus: "LOGGED_IN"
+    });
+  }
+
+  handleUnSuccessfulLogin() {
+    this.setState({
+      loggedInStatus: "NOT_LOGGED_IN"
+    });
+  }
+
   render() {
     return (
       <div className='container'>
       <Router>
           <div>
           
-            <NavigaionContainer />
+           <NavigaionContainer />
             <Switch>
               <Route exact path="/" component={Home} />
-              <Route path="/auth" component={Auth} />
+
+              <Route 
+              path="/auth" 
+              render={props => (
+                <Auth 
+                  {...props}
+                  handleSuccessfullLogin={this.handleSuccessfullLogin}
+                  handleUnSuccessfulLogin={this.handleUnSuccessfulLogin}
+                />
+              )}
+            />
+
               <Route path="/about-me" component={About} />
               <Route path="/contact" component={Contact} />
               <Route path="/blog-me" component={Blog} />
